e69e1a71da895dde21650b6d000a102cc7b87f9e
Fiyin <fiyinaregbe@gmail.com>
2023-04-25 06:13:16 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+
+
+export default function App() {
+  return (
+    <main>
+      <About/>
+      {/* <Navbar/>
+      <Projects />
+      <Skills/>
+      <Testimonials/>
+      <Contact/> */}
+    </main>
+  )
+}
